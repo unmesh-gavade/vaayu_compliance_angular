ffd62c8c97835660de2fcbf423172f064cbfb135
MansiSawant <sawant.mansi@mahindra.com>
2019-08-23 16:25:32 +0530

    image fixes

--- src/app/views/driver-personal/driver-personal.component.ts
+++ src/app/views/driver-personal/driver-personal.component.ts
@@ -10,7 +10,7 @@ export class DriverPersonalComponent implements OnInit {
   valueOfButton = "Edit";
   isEditModeOn = false;
   isDropup = true;
-  imageURL="../assets/img/Doc.jpg";
+  imageURL="./assets/img/Doc.jpg";
   editDriverPersonalForm: FormGroup;
   submitted = false;
   constructor(private formBuilder: FormBuilder) { }
