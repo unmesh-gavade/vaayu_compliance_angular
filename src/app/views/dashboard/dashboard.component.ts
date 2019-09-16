b4255ddca21fb50a41751880884238ece1d992eb
Umar Sayyed <umar.sayyed@fugetroncorp.com>
2019-09-16 14:34:36 +0530

    dashboard issues resolved

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,91 +1,94 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonService } from '../../services/common.service';
-import {DashboardService} from '../../services/dashboard.service';
+import { DashboardService } from '../../services/dashboard.service';
 import { THIS_EXPR, ThrowStmt } from '@angular/compiler/src/output/output_ast';
 import { AuthService } from '../../auth/auth.service';
 import { Router } from '@angular/router';
+import {AppConst} from '../../const/appConst';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.sass']
 })
 export class DashboardComponent implements OnInit {
+  customDate = new Date();
+  serverDateFormat = AppConst.SERVER_DATE_TIME_FORMAT;
   status: boolean = false;
   dashboardList: Object;
-  baList:{};
-  tatList:{};
-  isDriverSelected= true;
+  baList: {};
+  tatList: {};
+  isDriverSelected = true;
   resource_type: String;
-  tat_type:String;
+  tat_type: String;
 
-  clickEvent(){
-      this.status = !this.status;       
+  clickEvent() {
+    this.status = !this.status;
   }
-    search_res : any = '';
-    public search:any = '';
-    locked: any[] = [];
-    constructor(public commonService:CommonService, public Dashboard:DashboardService, private authService:AuthService,private router: Router,) { }
+  search_res: any = '';
+  public search: any = '';
+  locked: any[] = [];
+  constructor(public commonService: CommonService, public Dashboard: DashboardService, private authService: AuthService, private router: Router, ) { }
 
   ngOnInit() {
     this.authService.checkLogin();
-      this.locked = [
-        {ba_legal_name: 'Rajesh Singh', resource_id: 'Satish Tour & Travel', licence_number: 'AP265HDG236434', gender: 'Male', registeredby: 'Rushi Indulekar', induction_status: 'registered',date_of_registration : '07 July 2019 | 08:45 PM ',Action : 'VERIFY'},
-        {ba_legal_name: 'Rahul Rao', resource_id: 'Satish Tour & Travel', licence_number: 'AP265HDG236434', gender: 'Male', registeredby: 'Rushi Indulekar', induction_status: 'registered',date_of_registration : '07 July 2019 | 08:45 PM ',Action : 'VERIFY'},
-        {ba_legal_name: 'Praveen Kumar ', resource_id: 'Satish Tour & Travel', licence_number: 'AP265HDG236434', gender: 'Male', registeredby: 'Rushi Indulekar', induction_status: 'registered',date_of_registration : '07 July 2019 | 08:45 PM ',Action : 'VERIFY'},
-        {ba_legal_name: 'Rupali Ram ', resource_id: 'Satish Tour & Travel', licence_number: 'AP265HDG236434', gender: 'Female', registeredby: 'Rushi Indulekar', induction_status: 'registered',date_of_registration : '07 July 2019 | 08:45 PM ',Action : 'VERIFY'},
-        {ba_legal_name: 'Purab Kadam', resource_id: 'Satish Tour & Travel', licence_number: 'AP265HDG236434', gender: 'Male', registeredby: 'Rushi Indulekar', induction_status: 'registered',date_of_registration : '07 July 2019 | 08:45 PM ',Action : 'VERIFY'},
-        {ba_legal_name: 'Yagya Das', resource_id: 'Satish Tour & Travel', licence_number: 'AP265HDG236434', gender: 'Male', registeredby: 'Rushi Indulekar', induction_status: 'registered',date_of_registration : '07 July 2019 | 08:45 PM ',Action : 'VERIFY'},
-        {ba_legal_name: 'Pramod Achari', resource_id: 'Satish Tour & Travel', licence_number: 'AP265HDG236434', gender: 'Male', registeredby: 'Rushi Indulekar', induction_status: 'registered',date_of_registration : '08 July 2019 | 08:45 PM ',Action : 'VERIFY'},
+    this.locked = [
+      { ba_legal_name: 'Rajesh Singh', resource_id: 'Satish Tour & Travel', licence_number: 'AP265HDG236434', gender: 'Male', registeredby: 'Rushi Indulekar', induction_status: 'registered', date_of_registration: '07 July 2019 | 08:45 PM ', Action: 'VERIFY' },
+      { ba_legal_name: 'Rahul Rao', resource_id: 'Satish Tour & Travel', licence_number: 'AP265HDG236434', gender: 'Male', registeredby: 'Rushi Indulekar', induction_status: 'registered', date_of_registration: '07 July 2019 | 08:45 PM ', Action: 'VERIFY' },
+      { ba_legal_name: 'Praveen Kumar ', resource_id: 'Satish Tour & Travel', licence_number: 'AP265HDG236434', gender: 'Male', registeredby: 'Rushi Indulekar', induction_status: 'registered', date_of_registration: '07 July 2019 | 08:45 PM ', Action: 'VERIFY' },
+      { ba_legal_name: 'Rupali Ram ', resource_id: 'Satish Tour & Travel', licence_number: 'AP265HDG236434', gender: 'Female', registeredby: 'Rushi Indulekar', induction_status: 'registered', date_of_registration: '07 July 2019 | 08:45 PM ', Action: 'VERIFY' },
+      { ba_legal_name: 'Purab Kadam', resource_id: 'Satish Tour & Travel', licence_number: 'AP265HDG236434', gender: 'Male', registeredby: 'Rushi Indulekar', induction_status: 'registered', date_of_registration: '07 July 2019 | 08:45 PM ', Action: 'VERIFY' },
+      { ba_legal_name: 'Yagya Das', resource_id: 'Satish Tour & Travel', licence_number: 'AP265HDG236434', gender: 'Male', registeredby: 'Rushi Indulekar', induction_status: 'registered', date_of_registration: '07 July 2019 | 08:45 PM ', Action: 'VERIFY' },
+      { ba_legal_name: 'Pramod Achari', resource_id: 'Satish Tour & Travel', licence_number: 'AP265HDG236434', gender: 'Male', registeredby: 'Rushi Indulekar', induction_status: 'registered', date_of_registration: '08 July 2019 | 08:45 PM ', Action: 'VERIFY' },
     ];
-    this.Dashboard.getBaList().subscribe(res=>{
-      this.baList =res['data']['list'] ;
+    this.Dashboard.getBaList().subscribe(res => {
+      this.baList = res['data']['list'];
       console.log(this.baList);
     });
-    this.Dashboard.getDashboardTats().subscribe(tats=>{
-    this.tatList= tats['data']['tat_list'];
-    console.log(this.tatList);
-    
+    this.Dashboard.getDashboardTats().subscribe(tats => {
+      this.tatList = tats['data']['tat_list'];
+      console.log(this.tatList);
+
     });
 
-    if(this.isDriverSelected){this.resource_type='drivers'}
-    else{this.resource_type='vehicles'};
-    this.tat_type='draft';
-  //   var data = {
-  //     "resource_type": this.resource_type,
-  //     "search_by_tat":'new_request',
-  //     "search_by_name":'',
-  //     "start_page_index":0,
-  //     "record_per_page":10
-  //  }
-  //  this.Dashboard.getDashboardList(data).subscribe(res=>{
-  //   this.dashboardList = res['data']['filterData'];
-  //   console.log(this.dashboardList);
-  // })
-   this.onsubmit();
+    if (this.isDriverSelected) { this.resource_type = 'drivers' }
+    else { this.resource_type = 'vehicles' };
+    this.tat_type = 'draft';
+    //   var data = {
+    //     "resource_type": this.resource_type,
+    //     "search_by_tat":'new_request',
+    //     "search_by_name":'',
+    //     "start_page_index":0,
+    //     "record_per_page":10
+    //  }
+    //  this.Dashboard.getDashboardList(data).subscribe(res=>{
+    //   this.dashboardList = res['data']['filterData'];
+    //   console.log(this.dashboardList);
+    // })
+    this.onsubmit();
   }
 
-  onsubmit(){
+  onsubmit() {
     console.log('in sumbit');
     console.log(this.isDriverSelected);
     console.log(this.resource_type);
     var data = {
       "resource_type": this.resource_type,
-      "search_by_tat":this.tat_type,
-      "search_by_name":'',
-      "start_page_index":0,
-      "record_per_page":10
-   }
+      "search_by_tat": this.tat_type,
+      "search_by_name": '',
+      "start_page_index": 0,
+      "record_per_page": 10
+    }
 
-   
-   this.Dashboard.getDashboardList(data).subscribe(res=>{
-     console.log('in dash');
-     console.log(res);
-    this.dashboardList = res['data']['filterData'];
-    console.log(this.dashboardList);
-  })
-    
+
+    this.Dashboard.getDashboardList(data).subscribe(res => {
+      console.log('in dash');
+      console.log(res);
+      this.dashboardList = res['data']['filterData'];
+      console.log(this.dashboardList);
+    })
+
   };
-  Verify(resource_id,resource_type) {
+  Verify(resource_id, resource_type) {
     console.log(resource_id);
     alert(resource_type);
     if (resource_type == 'drivers') {
@@ -98,30 +101,32 @@ export class DashboardComponent implements OnInit {
     }
 
   };
-    onToolbarMenuToggle(){
-      console.log('toggle', this.commonService.isMenuOpen);
-      this.commonService.toggleMenu();
-    }
-    private pageSize: number = 5;
-    search_bt(query){
-      this.search_res = query;
-    }
-    selectDriver() {
-      this.resource_type = 'drivers';
-      this.onsubmit();
-          return;
-      }
-      selectVehicle() {
-        this.isDriverSelected = false;
-        this.resource_type = 'vehicles';
-        this.onsubmit();
-            return;
-        }
-        getTatType(tatType){
-          this.tat_type=tatType;
-          alert(this.tat_type);
-          this.onsubmit();
-        }
+  onToolbarMenuToggle() {
+    console.log('toggle', this.commonService.isMenuOpen);
+    this.commonService.toggleMenu();
   }
+  private pageSize: number = 5;
+  search_bt(query) {
+    this.search_res = query;
+  }
+
+  selectDriver() {
+    this.isDriverSelected = true;
+    this.resource_type = 'drivers';
+    this.onsubmit();
+
+  }
+  selectVehicle() {
+    this.isDriverSelected = false;
+    this.resource_type = 'vehicles';
+    this.onsubmit();
+
+  }
+  getTatType(tatType) {
+    this.tat_type = tatType;
+    alert(this.tat_type);
+    this.onsubmit();
+  }
+}
 
 
