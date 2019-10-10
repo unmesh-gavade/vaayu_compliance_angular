3d495ec1e519cd500a40531a20a374df1124606c
MansiSawant <sawant.mansi@mahindra.com>
2019-10-10 18:34:48 +0530

    external lib for popup

---
+++ src/app/views/confirm-component/confirm.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { DialogComponent, DialogService } from "../../../libs/ng2-bootstrap-modal";
+export interface ConfirmModel {
+  title:string;
+  message:string;
+}
+@Component({  
+    selector: 'confirm',
+    template: `<div class="modal-dialog">
+                <div class="modal-content">
+                   <div class="modal-header">
+                     <h4 class="modal-title">{{title || 'Confirm'}}</h4>
+                   </div>
+                   <div class="modal-body">
+                     <p>{{message || 'Are you sure?'}}</p>
+                   </div>
+                   <div class="modal-footer">
+                     <button type="button" class="btn btn-primary" (click)="confirm()">Yes</button>
+                     <button type="button" class="btn btn-default" (click)="close()" >Cancel</button>
+                   </div>
+                 </div>
+              </div>`
+})
+
+export class ConfirmComponent extends DialogComponent<ConfirmModel, boolean> implements ConfirmModel {
+  title: string;
+  message: string;
+  constructor(dialogService: DialogService) {
+    super(dialogService);
+  }
+  confirm() {
+    // we set dialog result as true on click on confirm button, 
+    // then we can get dialog result from caller code 
+    this.result = true;
+    this.close();
+  }
+}
