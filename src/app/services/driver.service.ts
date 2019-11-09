4ff288216e72f6e42f948589fd36e730cff445c8
Umar Sayyed <sayyed25507@gmail.com>
2019-11-09 19:14:31 +0530

    driver vehicle doc not showing issue fixed

--- src/app/services/driver.service.ts
+++ src/app/services/driver.service.ts
@@ -1,26 +1,27 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { ConstantService } from './constant.service';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { ConstantService } from "./constant.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DriverService {
-
   public baseUrl: String = "";
 
-  constructor(private http: HttpClient, public constService : ConstantService) {
-    this.baseUrl= constService.serverUrl;
-   }
+  constructor(private http: HttpClient, public constService: ConstantService) {
+    this.baseUrl = constService.serverUrl;
+  }
 
-   getDriverDetails(data) {
-     return this.http.post('http://ec2-13-233-214-215.ap-south-1.compute.amazonaws.com:8001/api/v1/' + 'getDetails',data);
-   }
-   updateDriverDetails(data) {
-    return this.http.post(this.baseUrl +'saveDetails',data)
+  getDriverDetails(data) {
+    return this.http.post(
+      "http://ec2-13-233-214-215.ap-south-1.compute.amazonaws.com:8001/api/v1/" + "getDetails",
+      data
+    );
+  }
+  updateDriverDetails(data) {
+    return this.http.post(this.baseUrl + "saveDetails", data);
   }
   getSiteList() {
-    return this.http.post('http://ec2-13-233-214-215.ap-south-1.compute.amazonaws.com/getAllSiteList', '');
+    return this.http.post("http://ec2-13-233-214-215.ap-south-1.compute.amazonaws.com/getAllSiteList", "");
   }
-
 }
